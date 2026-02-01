b1bac9ef99227c33664b3869a2e0aa13d36fda4a
Rahul Somasundaram <rsomasundaram1@wayfair.com>
2026-02-01 15:05:27 +0530

    updated default path

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 import ResumeViewer from "./components/ResumeViewer";
 
 const queryClient = new QueryClient();
@@ -19,7 +18,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/resume" element={<ResumeViewer />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Index />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
